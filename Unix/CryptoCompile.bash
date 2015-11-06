2b866c8484037c844d3f2742841835e84d7b6200
JonWBedard <JonWBedard@gmail.com>
2015-11-06 12:04:32 -0800

    Removed non-cmake builds

--- Unix/CryptoCompile.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir Debug
-
-cd Debug
-g++ -std=c++11 -c -w ../*.cpp -I ../../Datastructures -I ../../UnitTest
-
-ar rvs CryptoGateway.a *.o
-cd ..
--- Unix/CryptoGatewayUnixBuild.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-/bin/sh CryptoUnixDeconstruct.bash
-cd ..
-cp -r Unix/CryptoCompile.bash CryptoCompile.bash
-cd Unix
