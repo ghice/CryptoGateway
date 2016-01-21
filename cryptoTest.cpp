9b9df3f203d04427fce0fde2f7740de2d1fb824e
JonWBedard <JonWBedard@gmail.com>
2016-01-21 08:27:54 -0800

    Added supressed tests back

--- cryptoTest.cpp
+++ cryptoTest.cpp
@@ -1,5 +1,5 @@
 //Primary author: Jonathan Bedard
-//Confirmed working: 1/19/2016
+//Confirmed working: 1/21/2016
 
 #ifndef CRYPTO_TEST_CPP
 #define CRYPTO_TEST_CPP
@@ -21,13 +21,13 @@ using namespace test;
     CryptoGatewayLibraryTest::CryptoGatewayLibraryTest():
         libraryTests("CryptoGateway")
     {
-        /*pushSuite(os::smart_ptr<testSuite>(new C_BaseTenSuite(),os::shared_type));
+        pushSuite(os::smart_ptr<testSuite>(new C_BaseTenSuite(),os::shared_type));
         pushSuite(os::smart_ptr<testSuite>(new BasicNumberTest(),os::shared_type));
 		pushSuite(os::smart_ptr<testSuite>(new IntegerTest(),os::shared_type));
         pushSuite(os::smart_ptr<testSuite>(new xorTestSuite(),os::shared_type));
 		pushSuite(os::smart_ptr<testSuite>(new RC4HashTestSuite(),os::shared_type));
 		pushSuite(os::smart_ptr<testSuite>(new RC4StreamTestSuite(),os::shared_type));
-		pushSuite(os::smart_ptr<testSuite>(new cryptoFileTestSuite(),os::shared_type));*/
+		pushSuite(os::smart_ptr<testSuite>(new cryptoFileTestSuite(),os::shared_type));
         pushSuite(os::smart_ptr<testSuite>(new RSASuite(),os::shared_type));
     }
 
